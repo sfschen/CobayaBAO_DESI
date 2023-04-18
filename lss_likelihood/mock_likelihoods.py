39ec1320ee2c936ed4392f85d6fee693e1a4af6a
sfschen <shifan_chen@berkeley.edu>
2023-04-18 10:00:43 -0700

    Cleaning house.

--- lss_likelihood/mock_likelihoods.py
+++
@@ -1,8 +0,0 @@
-from reciso_likelihood import RecIsoLikelihood
-from recsym_likelihood import RecSymLikelihood
-
-class Martin_RecIso(RecIsoLikelihood):
-    pass
-
-class Martin_RecSym(RecSymLikelihood):
-    pass
